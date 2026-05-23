16025f0c4a8602c423b73fd160c6909e3a35ece4
Suvadip Kushari <kusharisubhadip@gmail.com>
2026-05-24 01:28:44 +0530

    Update: validate-parentheses - submission-0

--- Data Structures & Algorithms/validate-parentheses/submission-0.py
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -2,19 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         match = {
             ')': '(',
-            '}': '{',
-            ']': '['
+            ']': '[',
+            '}': '{'
         }
         stack = []
         for ch in s:
-            if ch in match.values():
-                stack.append(ch)
             if ch in match:
-                if not stack:
+                if not stack or stack[-1] != match[ch]:
                     return False
-                elif match[ch] != stack[-1]:
-                    return False
-                else:
-                    stack.pop()
+                stack.pop()
+            else:
+                stack.append(ch)
         return not stack
                 
